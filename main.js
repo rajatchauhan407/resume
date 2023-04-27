601fc8fcc13d4982dcc9378a2bf2dbea78a3df28
Rajat Chauhan <38553891+rajatchauhan407@users.noreply.github.com>
2023-04-27 13:15:34 -0700

    Check (#4)
    * lkajdf
    
    * check for pt

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
     console.log("Hey");
     console.log("Hey");
 }
+{
+    console.log("hello");
+    console.log("Hey");
+    console.log("Hey");
+}
 (function script(){
     console.log("Think of the resume in a way that you think");
 }());
