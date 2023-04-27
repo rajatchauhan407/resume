b7a04b106bc3d8c562b6aa431c3f3201203ce875
Rajat <rajatchauhan1323@gmail.com>
2023-04-27 12:21:25 -0700

    function script

--- main.js
+++ main.js
@@ -9,3 +9,6 @@
 (function script(){
     console.log("Think of the resume in a way that you think");
 }());
+(function script(){
+    console.log("Think of the resume in a way that you think");
+}());
